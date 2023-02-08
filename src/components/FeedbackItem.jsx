80a5fe3a1256e3078e29f67da730eaa57c88b5e1
Gaurav Singh <sgaurav4683@gmail.com>
2023-02-08 09:33:40 +0530

    card component created

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,14 @@
+
+import Card from "./Shared/Card"
+
+function FeedbackItem({item}) {
+  return (
+    <Card>
+    <div className='num-display'>{item.rating}</div>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
+
+
+export default FeedbackItem

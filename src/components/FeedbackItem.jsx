60ba5ef76fd04a2023698832ee95224df5b232b9
Gaurav Singh <sgaurav4683@gmail.com>
2023-02-08 13:27:42 +0530

    reverse for card added

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,18 @@
 
 import Card from "./Shared/Card"
+import PropTypes from 'prop-types'
 
 function FeedbackItem({item}) {
   return (
-    <Card>
+    <Card >
     <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
 }
 
+FeedbackItem.propTypes={
+  item:PropTypes.object.isRequired,
+}
 
 export default FeedbackItem
--- src/components/Shared/Card.jsx
+++ src/components/Shared/Card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import PropTypes  from 'prop-types'
+function Card({children,reverse}) {
+  // return (
+  //    <div className={`card ${reverse && 'reverse'}`}>{children}</div> 
+  //   //conditional statementto apply a class : apply css for class but if there is reverse then apply css of reverse [THIS IS AN EXAMPLE OF CONDITIONAL CLASS]
+  // )
 
-function Card({children}) {
   return (
-    <div className='card'>{children}</div>
+    //below is an example of conditional styling for the code same as above
+    <div className="card" style={{
+      backgroundColor:reverse ?'rgba(0,0,0,0.4)' : '#fff',
+      color:reverse ?'#fff' : 'rgba(0,0,0,0.4)'
+    }}>{children}</div>
+    //if reverse then we want background color to be black else white, same for text color
   )
+}
+Card.defaultProps={
+  reverse:true,
+}
+Card.propTypes={
+children:PropTypes.node.isRequired,
+reverse:PropTypes.bool,
 }
 
 export default Card

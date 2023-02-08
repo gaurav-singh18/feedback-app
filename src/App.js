016b4fb5402109ff740125d5dc72361b888ff605
Gaurav Singh <sgaurav4683@gmail.com>
2023-02-08 14:28:52 +0530

    delete button and function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback,setFeedback]=useState(FeedbackData)
+  const deleteFeedback=(id) =>{
+    if(window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter((item)=> item.id!==id))
+    }//using a higher order function:filter , it returns an array minus the one we are deleting
+  }
   return (
     <>
         <Header />
         <div className='container'>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
     </>
         
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
 
 import Card from "./Shared/Card"
 import PropTypes from 'prop-types'
-
-function FeedbackItem({item}) {
+import {  FaTrashAlt } from 'react-icons/fa';
+function FeedbackItem({item,handleDelete}) {
   return (
     <Card >
     <div className='num-display'>{item.rating}</div>
+    <button className="close" onClick={()=>handleDelete(item.id)}>
+      <FaTrashAlt color='white'/>
+    </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
@@ -16,3 +19,4 @@ FeedbackItem.propTypes={
 }
 
 export default FeedbackItem
+

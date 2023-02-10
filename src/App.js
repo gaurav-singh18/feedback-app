6c0089bc522611276721405934a7768d56132598
Gaurav Singh <sgaurav4683@gmail.com>
2023-02-10 10:41:21 +0530

    links added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage';
+import AboutIconLink from "./components/AboutIconLink";
 
 
 
@@ -39,6 +40,7 @@ function App() {
         <Route path="/about" element={<AboutPage />}/>
         </Routes>
         </div>
+        <AboutIconLink />
     </BrowserRouter>
         
   )
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/Shared/Card'
+import { Link } from 'react-router-dom'
 
 function AboutPage() {
   return (
@@ -7,7 +8,7 @@ function AboutPage() {
     <h1>About Page</h1>
        <p> This is a react app to leave feedback for a product or service</p>
        <p>
-        <a href='/'>Home</a>
+        <Link to='/'>Home</Link>
        </p>
     </Card>
   )
